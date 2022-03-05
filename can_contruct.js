7698359677a1437e061f2a2f722093e1866d0a69
JosephOlabode <josepholabode17@gmail.com>
2022-03-05 15:42:16 +0100

    implemented the count construct

--- can_contruct.js
+++ can_contruct.js
@@ -1,14 +1,17 @@
-function canConstruct(target, wordBank) {
+function canConstruct(target, wordBank, memo = {}) {
+    if(target in memo) return memo[target];
     if(target == '') return true;
 
     for(let word of wordBank) {
         if(target.indexOf(word) == 0) {
             const suffix = target.slice(word.length);
-            if(canConstruct(suffix, wordBank)) {
+            if(canConstruct(suffix, wordBank, memo)) {
+                memo[target] = true;
                 return true;
             }
         }
     }
 
+    memo[target]  = false;
     return false;
 }
