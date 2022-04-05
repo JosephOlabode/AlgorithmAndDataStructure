f4fba2c49de0d102ee4717cd7f0c7ca72dcb661a
JosephOlabode <josepholabode17@gmail.com>
2022-04-05 10:16:44 +0100

    implemented the container with most water capacity problem

--- containerWithMostWater.js
+++ containerWithMostWater.js
@@ -1,3 +1,4 @@
+// Time: O(n ^ 2), Space: O(1)
 function getContainerWithMostWaterCapacity(array) {
     //const containerMap = {};
     let max = 0;
@@ -19,5 +20,24 @@ function getContainerWithMostWaterCapacity(array) {
     return max;
 }
 
-const containerArray = [7, 1, 2, 3, 9];
-console.log(getContainerWithMostWaterCapacity(containerArray));
+// Time: O(n), Space: O(1)
+function getContainerWithMostWaterCapacity2(array) {
+    let i = 0, j = array.length - 1, maxArea = 0;
+
+    while(i <= j) {
+        const height = Math.min(array[i], array[j]);
+        const width = j - i;
+        const area = height * width;
+        maxArea = Math.max(maxArea, area);
+
+        if(array[i] <= array[j]) {
+            i++;
+        } else {
+            j--;
+        }
+    }
+    return maxArea;
+}
+
+const containerArray = [4, 8, 1, 2, 3, 9]//[7, 1, 2, 3, 9];
+console.log(getContainerWithMostWaterCapacity2(containerArray));
