b2f76d50ccb0abeda571c4f2907b3367dbe2b060
JosephOlabode <josepholabode17@gmail.com>
2022-04-16 03:45:53 +0100

    implemented a linked list with reversing too

---
+++ reverse_a_linklist.js
@@ -0,0 +1,115 @@
+
+
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkList {
+
+    constructor(headNodeData) {
+        this.headNode = new Node(headNodeData);
+    }
+
+    insertNodeAtEnd(data) {
+        let currentNode = this.headNode;
+        while(currentNode.next !== null) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = new Node(data);
+    }
+
+    insertInBetween(position, data) {
+        let currentNode = this.headNode;
+
+        while(currentNode.next !== null && currentNode.data !== position) {
+            currentNode = currentNode.next;
+        }
+
+        if(currentNode.next !== null && currentNode.data == position){
+            let newNode = new Node(data);
+            let nextNode = currentNode.next;
+            currentNode.next = newNode;
+            newNode.next = nextNode;
+        } else {
+            currentNode.next = new Node(data);
+        }
+    }
+
+    removeNode(data) {
+        let currentNode = this.headNode;
+        let previousNode = null;
+
+        while(currentNode.next !== null && currentNode.data !== data) {
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+
+        previousNode.next = currentNode.next;
+    }
+
+    removeNodeFromEnd() {
+        let currentNode = this.headNode;
+        let previousNode = null;
+
+        while(currentNode.next !== null ) {
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+
+        previousNode.next = null;
+    }
+
+    reverseNodes() {
+        let currentNode = this.headNode;
+        let nextNode = this.headNode.next;
+        let tempNode = null
+
+        while(nextNode.next !== null) {
+            tempNode = nextNode.next;
+            nextNode.next = currentNode;
+            currentNode = nextNode;
+            nextNode = tempNode;
+        }
+        nextNode.next = currentNode;
+        currentNode = nextNode;
+
+        this.headNode.next = null;
+        this.headNode = currentNode;
+    }
+
+    printNodes() {
+        let currentNode = this.headNode;
+        while(currentNode.next !== null) {
+            console.log(currentNode.data);
+            currentNode = currentNode.next;
+        }
+        console.log(currentNode.data);
+    }
+
+}
+
+function reverseNodes() {
+    const list = new LinkList(1);
+    list.insertNodeAtEnd(2);
+    list.insertNodeAtEnd(3);
+    // list.insertNodeAtEnd(4);
+     list.insertNodeAtEnd(5);
+     list.insertInBetween(3, 4)
+     //list.removeNodeFromEnd();
+     list.insertNodeAtEnd(6);
+     list.removeNode(2);
+
+    console.log('Before Reverse Operation');
+    list.printNodes();
+    console.log('\n')
+    list.reverseNodes();
+    console.log('After Reverse Operation');
+    list.printNodes();
+}
+
+reverseNodes();
+
