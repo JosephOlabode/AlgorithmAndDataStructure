06cabb03b49e5fca5cc34d67397a6c3decf4a254
JosephOlabode <josepholabode17@gmail.com>
2022-04-19 03:20:56 +0100

    implemented floyd algorithm of finding a cycle

--- cycle_detection_linklist.js
+++ cycle_detection_linklist.js
@@ -1,3 +1,4 @@
+// Time: O(n), Space: O(n)
 function findCycle(head) {
     let currentNode = head;
     let seenNodes = new Set();
@@ -9,4 +10,30 @@ function findCycle(head) {
         seenNodes.add(currentNode);
         currentNode = currentNode.next;
     }
+    return currentNode;
+}
+
+// Floyd tortoise and hare algorithm
+// Time: O(n), Space: O(1)
+function findCycle2(head) {
+    let hare = head, tortoise = head;
+    while(true) {
+        hare = hare.next;
+        tortoise = tortoise.next;
+
+        if(hare === null || hare.next === null){
+            return false;
+        } else {
+            hare = hare.next;
+        }
+
+        if(tortoise === hare) break;
+    }
+
+    let p1 = head, p2 = tortoise;
+    while(p1 !== p2) {
+        p1 = p1.next;
+        p2 = p2.next;
+    }
+    return p1;
 }
