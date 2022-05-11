5035544a388704aac375ebeea7d7c8552ae6681d
JosephOlabode <josepholabode17@gmail.com>
2022-05-11 09:42:52 +0100

    implemented the level order traversal with each level set

--- level_order_traversal.js
+++ level_order_traversal.js
@@ -1,5 +1,6 @@
 
 let BST = require('./binary_search_tree.js');
+// Time: O(n), Space: O(n)
 function traverse_by_level(rootNode) {
     const res = [], q = [rootNode];
     while(q.length) {
@@ -13,10 +14,34 @@ function traverse_by_level(rootNode) {
     return res;
 }
 
+// Time: O(n), Space: O(n)
+const level_order = function(root) {
+    if(!root) return [];
+    const result = [];
+    const queue = [root];
+
+    while(queue.length) {
+        let length = queue.length, count = 0;
+        const currentLevelValues = [];
+
+        while(count < length) {
+            const currentNode = queue.shift();
+            currentLevelValues.push(currentNode.value);
+            if(currentNode.left) queue.push(currentNode.left)
+            if(currentNode.right) queue.push(currentNode.right);
+            count++;
+        }
+
+        result.push(currentLevelValues);
+        
+    }
+    return result;
+}
+
 const bst = new BST();
 bst.insert(5);
 bst.insert(4);
 bst.insert(7);
 bst.insert(3);
 
-console.log(traverse_by_level(bst.root));
+console.log(level_order(bst.root));
