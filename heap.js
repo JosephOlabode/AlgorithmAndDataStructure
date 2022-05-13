764b32988feb23b3312d0e9a5d29cd4d67824615
JosephOlabode <josepholabode17@gmail.com>
2022-05-13 20:41:49 +0100

    add coment on how to generate right and left

--- heap.js
+++ heap.js
@@ -2,3 +2,5 @@
 // that the heap was received in the form of an array
 // parent = floor((indexOfNode - 1) / 2)
 // [ 50, 40, 25, 20, 35, 10, 15] the parent of node 10 will be 25
+// left = (indexOfNode * 2) + 1
+// right = (indexOfNode * 2) + 2
