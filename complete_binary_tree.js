8b17ebfbc4ae9639bafeab3b95b14026361f6e30
JosephOlabode <josepholabode17@gmail.com>
2022-05-13 11:05:34 +0100

    traverse a binary tree

---
+++ complete_binary_tree.js
@@ -0,0 +1,27 @@
+const BST = require('./binary_search_tree');
+const traverse = function(rootNode, nodeArray = []) {
+    if(!rootNode) return
+    nodeArray.push(rootNode.value);
+
+    if(rootNode.left) {
+        traverse(rootNode.left, nodeArray);
+    }
+    if(rootNode.right) {
+        traverse(rootNode.right, nodeArray);
+    }
+    return nodeArray;
+}
+
+const bst = new BST();
+bst.insert(11);
+bst.insert(7);
+bst.insert(12);
+bst.insert(5);
+bst.insert(9);
+bst.insert(4);
+bst.insert(13);
+bst.insert(8);
+bst.insert(10);
+
+console.log(traverse(bst.root));
+
