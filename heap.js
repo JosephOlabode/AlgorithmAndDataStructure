c907997d10790ab0d459de293ea26fe61f48692a
JosephOlabode <josepholabode17@gmail.com>
2022-05-13 20:38:35 +0100

    added comment on how to generate parent node of a node in max heap

---
+++ heap.js
@@ -0,0 +1,4 @@
+// formula for finding the parent of a node in max heap assuming
+// that the heap was received in the form of an array
+// parent = floor((indexOfNode - 1) / 2)
+// [ 50, 40, 25, 20, 35, 10, 15] the parent of node 10 will be 25
