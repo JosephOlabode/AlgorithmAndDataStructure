f71ed71065472a65c81866ad95aef07777671c7a
JosephOlabode <josepholabode17@gmail.com>
2022-05-15 18:19:51 +0100

    modified the question

--- navigating_2d_array.js
+++ navigating_2d_array.js
@@ -2,5 +2,5 @@
  * Question: Give a 2D array containing only 1's(land) and O's(water)
  * count the number of islands.
  * 
- * An island is land connected horizontally or vertically.
+ * An island is land connected horizontally or vertically. but not diagonally
  */
