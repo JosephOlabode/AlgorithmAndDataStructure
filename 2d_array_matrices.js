59787b933f3c49173bf1cf9fc2d0e9a45a3a57d8
JosephOlabode <josepholabode17@gmail.com>
2022-05-15 17:03:17 +0100

    partial implementation of the dfs traversal of the 2d array

--- 2d_array_matrices.js
+++ 2d_array_matrices.js
@@ -6,3 +6,27 @@ const normal2DTraversal = function(array) {
         }
     }
 }
+
+const dfs2DTraversal = function(array) {
+    const direction = [
+        [-1, 0], //up
+        [0, 1], //right
+        [1, 0], //down
+        [0, -1] //left
+    ];
+
+}
+
+const traversalDFS = function (matrix) {
+    const seen = new Array(matrix.length).fill(0)
+    .map(() => new Array(matrix[0].length).fill(false));
+
+    const values = [];
+
+    dfs(matrix, 0, 0, seen, values);
+    return values;
+}
+
+const dfs = function(matrix, row, col, seen, values) {
+
+}
