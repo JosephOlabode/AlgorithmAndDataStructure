f1c2b101b42d96a9c5afe045c5476dcaf5bfa15d
JosephOlabode <josepholabode17@gmail.com>
2022-05-15 15:24:27 +0100

    implemented the size, isempty and peek

--- heap.js
+++ heap.js
@@ -4,3 +4,24 @@
 // [ 50, 40, 25, 20, 35, 10, 15] the parent of node 10 will be 25
 // left = (indexOfNode * 2) + 1
 // right = (indexOfNode * 2) + 2
+
+class PriorityQueue {
+    constructor(comparator = (a, b) => a > b) {
+        this._heap = [];
+        this._comparator = comparator;
+    }
+
+    size() {
+        return this._heap.length;
+    }
+
+    isEmpty() {
+        return this._heap.length == 0 ? true: false;
+    }
+
+    peek() {
+        if(!this.isEmpty()) {
+            return this._heap[0]
+        }
+    }
+}
