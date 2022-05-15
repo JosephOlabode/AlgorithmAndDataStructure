bf095a3dff5b8bceb3caccc9f059147020bda136
JosephOlabode <josepholabode17@gmail.com>
2022-05-15 19:08:18 +0100

    added a test matrix to verify the correctness of the code

--- navigating_2d_array.js
+++ navigating_2d_array.js
@@ -55,3 +55,11 @@ const getNumberOfConnectedIsland = function (matrix) {
 
 }
 
+const matrix = [
+    [1,1,1,1,0],
+    [1,1,0,1,0],
+    [1,1,0,0,1],
+    [0,0,0,1,1]
+];
+
+console.log(getNumberOfConnectedIsland(matrix));
