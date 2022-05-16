5e7a0641422b49915e1e2d95249d5cf1a93c1b79
JosephOlabode <josepholabode17@gmail.com>
2022-05-16 19:09:54 +0100

    implemented the rotten orange code

--- 2d_array_rotten_oranges.js
+++ 2d_array_rotten_oranges.js
@@ -7,6 +7,69 @@
  * How many minutes must pass untill all oranges are rotten?
  */
 
+
+const directions = [
+    [-1, 0], [0, 1], [1, 0], [0, -1]
+];
+
+const ROTTEN = 2;
+const FRESH = 1;
+const EMPTY = 0;
+
+const orangesRotting = function(matrix) {
+    if(matrix.length === 0) return 0;
+
+    const queue = [];
+    let freshOranges = 0;
+
+    for(let row = 0; row < matrix.length; row++) {
+        for(let col = 0; col < matrix[0].length; col++) {
+            if(matrix[row][col] === ROTTEN) {
+                queue.push([row, col]);
+            }
+            if(matrix[row][col] === FRESH) {
+                freshOranges++;
+            }
+        }
+    }
+
+    let currentQueueSize = queue.length;
+    let minutes = 0;
+    while(queue.length > 0) {
+        if(currentQueueSize == 0) {
+            minutes++;
+            currentQueueSize = queue.length;
+        }
+
+        const currentOrange = queue.shift();
+        currentQueueSize--;
+        const row = currentOrange[0];
+        const col = currentOrange[1];
+
+        for(let i = 0; i < directions.length; i++) {
+            const currentDir = directions[i];
+
+            const nextRow = currentDir[0] + row;
+            const nextCol = currentDir[1] + col;
+
+            if(nextRow < 0 || nextRow >= matrix.length || nextCol < 0 || nextCol >= matrix[0].length) {
+                continue;
+            }
+
+            if(matrix[nextRow][nextCol] === FRESH) {
+                matrix[nextRow][nextCol] = 2;
+                freshOranges--;
+                queue.push([nextRow, nextCol]);
+            }
+        }
+    }
+
+    if(freshOranges > 0) return -1;
+
+    return minutes;
+}
+
+
 const matrix1 = [
     [2, 1, 1, 0, 0],
     [1, 1, 1, 0, 0],
