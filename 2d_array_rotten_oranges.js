63ca3173af6775af8b05d80635e79fa383e6e871
JosephOlabode <josepholabode17@gmail.com>
2022-05-16 13:12:54 +0100

    added the questions of the rotten oranges

---
+++ 2d_array_rotten_oranges.js
@@ -0,0 +1,8 @@
+/**
+ * Give a 2D array containing 0's (empty cell),
+ * 1's (fresh orange) and 2's (rotten orange).
+ * Every minute, all fresh orange immediately
+ * adjacent (4 directions) to rotten oranges will rot.
+ * 
+ * How many minutes must pass untill all oranges are rotten?
+ */
