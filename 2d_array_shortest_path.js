5a37f531f917af3b556d87d0d3fb808cd6b433c7
JosephOlabode <josepholabode17@gmail.com>
2022-05-16 19:30:33 +0100

    added the question

---
+++ 2d_array_shortest_path.js
@@ -0,0 +1,9 @@
+/**
+ * Given a 2D array containing -1's (walls), O's(gates) and
+ * INF's (empty room). Fill each empty room with the number
+ * of steps to the nearest gate
+ * 
+ * 
+ * If it is impossible to reach a gate, leave INF as the value.
+ * INF is eqaul to 2147483647
+ */
