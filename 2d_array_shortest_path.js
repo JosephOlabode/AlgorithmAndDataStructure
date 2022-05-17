64396c0d6ed170cacd629e31e2d256232632e84e
JosephOlabode <josepholabode17@gmail.com>
2022-05-17 12:36:29 +0100

    implemented the solution

--- 2d_array_shortest_path.js
+++ 2d_array_shortest_path.js
@@ -32,6 +32,19 @@ const wallsAndGates = function(matrix) {
 }
 
 
+const dfs = function(matrix, row, col, currentStep){
+    if(row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length || currentStep > matrix[row][col]) {
+        return;
+    }
+
+    matrix[row][col] = currentStep;
+
+    for(let i = 0; i < directions.length; i++) {
+        const currentDir = directions[i];
+        dfs(matrix, row + currentDir[0], col + currentDir[1], currentStep + 1);
+    }
+}
+
 const matrix1 = [
     [INF, -1, 0, INF],
     [INF, INF, INF, -1],
