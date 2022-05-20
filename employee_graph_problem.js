b2d740ce60110bb626c7d2bbfed5428c1bc5ad42
JosephOlabode <josepholabode17@gmail.com>
2022-05-20 09:26:07 +0100

    added the steps for solving a graph problem

---
+++ employee_graph_problem.js
@@ -0,0 +1,6 @@
+/**
+ * STEPS TO SOLVING A GRAPH PROBLEM
+ * 1. Transform the statement into a structure
+ * 2. Develop the adjency representation from the structure -> either adjency list or adjency matrix
+ * 3. Choose the best traversal mechanism to use to get ur answer (bfs, dfs).
+ */
