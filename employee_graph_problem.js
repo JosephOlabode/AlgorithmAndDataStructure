a14f3a18707035d281b4c78873a9b079c5c3af8b
JosephOlabode <josepholabode17@gmail.com>
2022-05-22 11:40:13 +0100

    implemented the dfs traversal

--- employee_graph_problem.js
+++ employee_graph_problem.js
@@ -19,5 +19,16 @@ const numOfMinutes = function(n, headId, managers, informTime) {
 }
 
 const dfs = function (currentId, adjList, informTime) {
-    
+    if(adjList[currentId].length === 0) {
+        return 0;
+    }
+
+    let max = 0;
+    const subordinates = adjList[currentId];
+
+    for(let i = 0; i < subordinates.length; i++) {
+        max = Math.max(max, dfs(subordinates[i], adjList, informTime));
+    }
+
+    return max + informTime[currentId];
 }
