9f0b49de3008c578ac8435e8a8c1fbeaf634c4ba
JosephOlabode <josepholabode17@gmail.com>
2022-05-22 11:36:15 +0100

    implementing the employee function

--- employee_graph_problem.js
+++ employee_graph_problem.js
@@ -4,3 +4,20 @@
  * 2. Develop the adjency representation from the structure -> either adjency list or adjency matrix
  * 3. Choose the best traversal mechanism to use to get ur answer (bfs, dfs).
  */
+
+const numOfMinutes = function(n, headId, managers, informTime) {
+    const adjList = managers.map(() => []);
+
+    for(let e = 0; e < n; e++) {
+        const manager = managers[e];
+
+        if(manager === -1) continue;
+        adjList[manager].push(e);
+    }
+
+    return dfs(headId, adjList, informTime);
+}
+
+const dfs = function (currentId, adjList, informTime) {
+    
+}
