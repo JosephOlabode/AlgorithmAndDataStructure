6e7dca5919eafc3daa8d46970eec19ae0271bc25
JosephOlabode <josepholabode17@gmail.com>
2022-05-22 13:10:06 +0100

    solved the problem

--- graph_question2.js
+++ graph_question2.js
@@ -6,3 +6,37 @@
  * Given the total number of courses and an array of prerequisite pairs, return
  * if it is possible to finish all courses
  */
+
+const canFinish = function(n, prerequisite){
+    const adjList = new Array(n).fill(0).map(() => []);
+
+    for(let i = 0; i < prerequisite.length; i++) {
+        const pair = prerequisite[i];
+        adjList[pair[1]].push(pair[0]);
+    }
+
+    for(let v = 0; v < n; v++) {
+        const queue = [];
+        const seen = {};
+
+        for(let i = 0; i < adjList[v].length; i++) {
+            queue.push(adjList[v][i]);
+        }
+
+        while(queue.length) {
+            const current = queue.shift();
+            seen[current] = true;
+
+            if(current === v) return false;
+            const adjacent = adjList[current];
+
+            for(let i = 0; i < adjacent.length; i++) {
+                const next = adjacent[i];
+                if(!seen[next]) {
+                    queue.push(next);
+                }
+            }
+        }
+    }
+    return true;
+}
