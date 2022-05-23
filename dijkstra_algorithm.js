35c1041bd09ec7277a8374fc626776ab6b122a54
JosephOlabode <josepholabode17@gmail.com>
2022-05-23 08:07:24 +0100

    dijstra algorithm

---
+++ dijkstra_algorithm.js
@@ -0,0 +1,7 @@
+/**
+ * There are n network nodes labelled 1 to N.
+ * Given a times array, containing edges represented by arrays
+ * [u, v, w] where u  is the source node, v is the target node,
+ * and w is the time taken to travel from the source node to the
+ * target node.
+ */
