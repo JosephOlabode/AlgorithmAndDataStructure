474fc2ff4cd70fc795a7d0883435257fa3ad81f8
JosephOlabode <josepholabode17@gmail.com>
2022-05-24 08:06:02 +0100

    solved the problem in an unoptimized way

--- dynamic_programming.js
+++ dynamic_programming.js
@@ -6,3 +6,15 @@
  * two steps. Find the minimum cost to reach the top of the staircase.
  * Your first step can either be the first or second step
  */
+
+const minCostClimbingStairs = function(cost) {
+    const n = cost.length;
+    return Math.min(minCost(n-1, cost), minCost(n-2, cost));
+}
+
+const minCost = function(i, cost) {
+    if(i < 0) return 0;
+    if(i === 0 || i === 1) return cost[i];
+
+    return cost[i] + Math.min(minCost(i-1, cost), minCost(i-2, cost));
+}
