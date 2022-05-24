3c3ff5811b2c8973c4b2d5caae5e22ef4f4bb979
JosephOlabode <josepholabode17@gmail.com>
2022-05-24 08:25:02 +0100

    implemented the dynamic programming solution of it

--- dynamic_programming.js
+++ dynamic_programming.js
@@ -19,3 +19,23 @@ const minCost = function(i, cost) {
 
     return cost[i] + Math.min(minCost(i-1, cost), minCost(i-2, cost));
 }
+
+// this solution is using dynamic programming
+
+const minCostClimbingStairs2 = function(cost) {
+    const n = cost.length;
+    return Math.min(minCost2(n-1, cost, dp), minCost2(n-2, cost, dp));
+}
+
+const minCost2 = function(i, cost, dp) {
+    if(i < 0) return 0;
+    if(i === 0 || i === 1) return cost[i];
+
+    if(dp[i] !== undefined) {
+        return dp[i]
+    } else {
+        dp[i] = cost[i] + Math.min(minCost2(i-1, cost, dp), minCost2(i-2, cost, dp));
+    }
+
+    return dp[i];
+}
