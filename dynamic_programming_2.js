e1c819005cef4a8c28188b1ca83ad284f3c39215
JosephOlabode <josepholabode17@gmail.com>
2022-05-24 09:12:59 +0100

    added the question

---
+++ dynamic_programming_2.js
@@ -0,0 +1,11 @@
+/**
+ * On a give n * n chessboard, a knight piece will start at the
+ * r-th row and c-th column. The knight will attempt to make
+ * k moves.
+ * 
+ * A knight can move in 8 possible ways. Each move will choose
+ * one of these 8 at random. The knight continues moving until
+ * it finishes k moves or it moves off the chessboard. Return
+ * the probability that the knight is on the chessboard after
+ * it finishes moving.
+ */
