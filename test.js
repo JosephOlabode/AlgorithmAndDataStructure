c52dab88ca4317bde0eb71351c89b51da98b57f1
Olabode Joseph Oluwaseun <josepholabode17@gmail.com>
2022-12-02 19:37:58 +0100

    implemented reading of file path

--- test.js
+++ test.js
@@ -1,27 +1,44 @@
-const nemo = ['nemo'];
-const largeArray = new Array(100).fill('nemo');
-function findingNemo(array) {
-    for(let i = 0; i < array.length; i++) {
-        if(array[i] === 'nemo') {
-            console.log('Found NEMO!');
-        }
-    }
-}
-//findingNemo(largeArray);
+// const nemo = ['nemo'];
+// const largeArray = new Array(100).fill('nemo');
+// function findingNemo(array) {
+//     for(let i = 0; i < array.length; i++) {
+//         if(array[i] === 'nemo') {
+//             console.log('Found NEMO!');
+//         }
+//     }
+// }
+// //findingNemo(largeArray);
 
+// const obj = {
+//     name: 'Joseph',
+//     last_name: 'oLaboDe',
+//     state_of_origin: {
+//         label: 'Anambra',
+//         value: 'Anambra'
+//     }
+// }
+// const value = obj.state_of_origin.value.toLowerCase();
+// const newObj = {
+//     value: obj.state_of_origin.value.toLowerCase(),
+//     name: 'Joseph'
+// }
+// console.log(newObj);
+// console.log(JSON.stringify(obj));
+const path = require("path");
+const fs = require("fs");
+//joining path of directory
 
-const obj = {
-    name: 'Joseph',
-    last_name: 'oLaboDe',
-    state_of_origin: {
-        label: 'Anambra',
-        value: 'Anambra'
-    }
+async function getFilePaths() {
+  const directoryPath = path.join(__dirname, "Second");
+  const pathArray = [];
+
+  const files = await fs.promises.readdir(directoryPath);
+  for (let file of files) {
+    pathArray.push(`${directoryPath}/${file}`);
+  }
+
+  console.log(pathArray);
+  return pathArray;
 }
-const value = obj.state_of_origin.value.toLowerCase();
-const newObj = {
-    value: obj.state_of_origin.value.toLowerCase(),
-    name: 'Joseph'
-}
-console.log(newObj);
-console.log(JSON.stringify(obj));
+
+getFilePaths();
